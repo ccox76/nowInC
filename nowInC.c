3b5f517c87117bd542a1f6877cbfef243b4b0054
Cyrus Cox <55308997+ccox76@users.noreply.github.com>
2024-12-05 14:18:27 -0500

    add handling for wrong characters and too many characters in input

--- nowInC.c
+++ nowInC.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 
 void toBin(char c) {
+    int go = 0;
     if (c <= '9' && c >= '0') {
         c = c - '0';
-    } else {
+        go = 1;
+    } else if (c <= 'F' && c >= 'A') {
         c = c - 'A' + 10;
+        go = 1;
+    } else if (c == 'X') {
+        printf("ending...");
+    } else {
+        printf("char out of range.\n");
     }
-    
-    printf("Binary equivalent: ");
-    for (int i=3;i>=0;i--) {
-        printf("%d", c>>i&1);
+    if (go) {
+        printf("Binary equivalent: ");
+        for (int i=3;i>=0;i--) {
+            printf("%d", c>>i&1);
+        }
+        printf("\n");
     }
-    printf("\n");
 }
 
 int main() {
@@ -25,6 +34,9 @@ int main() {
 
         if (input[0] == 'X') {
             go = 0;
+        }
+        if (strlen(input) != 1) {
+            printf("Multiple chars entered. Try again\n");
         } else {
             toBin(input[0]);
         }
